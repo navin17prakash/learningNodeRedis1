c1d7eef14f9f503cd218851beab26a5bdadfa239
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-02-18 00:17:44 +0530

    Added server.js and require statements

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 var DbConnectionString = 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
-var redisConnectionString = '';
+var redisConnectionString = 'pub-redis-16736.us-east-1-3.6.ec2.redislabs.com';
+var redisHost = 16736 ;
 module.exports.redisURL = redisConnectionString;
 module.exports.dbURL=DbConnectionString;
+module.exports.redisHost = redisHost;
+
---
+++ server.js
@@ -0,0 +1,10 @@
+//require the modules for the project 
+
+var mongoose = require('mongoose');
+var express = require('express');
+var redis = require('redis');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var config = require('./config.js');
+
