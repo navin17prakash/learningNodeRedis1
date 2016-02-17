289ab939ab95b87183acf40b6f2a111f667ecf21
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-02-18 00:05:08 +0530

    Added config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+var DbConnectionString = 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
+var redisConnectionString = '';
+module.exports.redisURL = redisConnectionString;
+module.exports.dbURL=DbConnectionString;
