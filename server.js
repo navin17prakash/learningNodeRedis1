3723444941c4a722bf6a45cc7a63c501e6a0b056
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-02-18 00:34:52 +0530

    connected to redis

--- server.js
+++ server.js
@@ -8,3 +8,28 @@ var morgan = require('morgan');
 
 var config = require('./config.js');
 
+//Start the server
+
+//Connection to redis
+var redisConnect = function redisConnect(){
+ console.log('Attempting the redis connection');   
+var redisClient = redis.createClient(config.redisHost,config.redisURL);
+
+redisClient.on('connect',function connectedRedis(){
+    console.log('Successfuly connected to the Redis client')
+    
+});
+redisClient.auth('123456', function (error) {
+    if (error)
+    {
+        console.log('authentication failed');
+    }
+});
+
+redisClient.on('error',function connectedRedis(){
+    console.log('Failed to  connect to the Redis client')
+    
+});
+    } //end redisConnectFunction
+    
+redisConnect();
